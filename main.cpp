eef75859e50bb7cfecaf6b861ffd2af396a7b85e
taegwonlee <vlldbllv@naver.com>
2021-12-22 15:39:48 +0900

    세번째

--- main.cpp
+++ main.cpp
@@ -7,5 +7,14 @@ int main()
 {
 
 	cout << "Hello World" << endl;
+
+	int Height = 0;
+	int Base = 0;
+	Float Area = 0.0f;
+
+	Area = ((static_cast<Float>(Height) * static_cast<Float>(base)) / 2.0f;
+
+	cout << "Area" << endl;
+
 	return 0;
 }
